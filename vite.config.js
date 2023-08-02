757063c50c1e11fa3771a877ea854e1fefcceabf
Claudia M Cardona H <80611384+claumcarh@users.noreply.github.com>
2023-08-02 15:28:19 -0500

    Se configura vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-export default {
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   root: './src',
   build: {
     outDir: '../dist',
   },
-}
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require('autoprefixer')],
+    },
+  },
+});
